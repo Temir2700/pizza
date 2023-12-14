7405c1e3f348d952144d23fa78746dba2feb7df5
sakutaku <116337732+sakutaku@users.noreply.github.com>
2023-12-15 00:26:19 +0600

    Update MainHeader.tsx

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from "../../assets/images/Logo.png";
+import { useNavigate} from "react-router-dom";
 
 const MainHeader = () => {
+    const navigate = useNavigate();
+    const onLogoClick = () => {
+        navigate('/');
+    };
+    
     return (
         <header className="header">
             <div className="container">
-                <div className="logo">
+                <div className="logo" onClick={onLogoClick}>
                     <img src={logo} alt="Logo"/>
                 </div>
             </div>
